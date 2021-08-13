9d1f594e23371c80b969be6889c8048e2ff1f1f7
Vuong Huynh <mannhuynh@gmail.com>
2021-08-13 15:43:48 -0500

    Finalize Songs App

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -1,5 +1,5 @@
 // Create Action
-export const selectSong = () => {
+export const selectSong = (song) => {
 	// Return an action
 	return {
 		type: "SONG_SELECTED",
--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
+import SongDetail from "./SongDetail";
 import SongList from "./SongList";
 // import { selectSong } from "../actions";
 
 const App = () => {
 	return (
-		<h1>
-			<SongList />
-		</h1>
+		<div className="ui container grid">
+			<div className="ui row">
+				<div className="column eight wide">
+					<SongList />
+				</div>
+				<div className="column eight wide">
+					<SongDetail />
+				</div>
+			</div>
+		</div>
 	);
 };
 export default App;
